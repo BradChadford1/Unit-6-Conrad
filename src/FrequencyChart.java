277934055fed5ddad99b14fa617b0091ed1cd171
BradChadford1 <54950991+BradChadford1@users.noreply.github.com>
2020-02-03 09:23:21 -0800

    works bb!

--- src/FrequencyChart.java
+++ src/FrequencyChart.java
@@ -8,41 +8,47 @@ public class FrequencyChart {
         this.data = data;
     }
 
-    public String makeChart(){
+    public void makeChart(){
         int c1=0, c11=0, c21=0, c31=0, c41=0, c51=0, c61=0, c71=0, c81=0, c91=0;
         for (int n : data) {
             if ((n > 0) && (n < 101)) {
                 if (n <= 10)
                     c1++;
-                if (n <= 20)
+                if ((n <= 20) && (n > 10))
                     c11++;
-                if (n <= 30)
+                if ((n <= 30) && (n > 20))
                     c21++;
-                if (n <= 40)
+                if ((n <= 40) && (n > 30))
                     c31++;
-                if (n <= 50)
+                if ((n <= 50) && (n > 40))
                     c41++;
-                if (n <= 60)
+                if ((n <= 60) && (n > 50))
                     c51++;
-                if (n <= 70)
+                if ((n <= 70) && (n > 60))
                     c61++;
-                if (n <= 80)
+                if ((n <= 80) && (n > 70))
                     c71++;
-                if (n <= 90)
+                if ((n <= 90) && (n > 80))
                     c81++;
                 else
                     c91++;
             }
         }
 
-
-        String chart = plot(c1) + plot(c11);
-        return chart;
+        System.out.println("11-20 | " + plot(c1));
+        System.out.println("21-30 | " + plot(c11));
+        System.out.println("31-40 | " + plot(c21));
+        System.out.println("41-50 | " + plot(c31));
+        System.out.println("51-60 | " + plot(c41));
+        System.out.println("61-70 | " + plot(c51));
+        System.out.println("71-80 | " + plot(c61));
+        System.out.println("81-90 | " + plot(c71));
+        System.out.println("91-100| " + plot(c81));
     }
 
     private String plot(int freq) {
         StringBuilder star = new StringBuilder();
-        for (int i = 0; i == freq; i++) {
+        for (int i = 0; i < freq; i++) {
             star.append("*");
         }
         return star.toString();
@@ -66,8 +72,8 @@ public class FrequencyChart {
             data[i] = val;
         }
 
-        FrequencyChart = new FrequencyChart(data);
+        FrequencyChart chart = new FrequencyChart(data);
 
-        System.out.println(data.makeChart());
+        chart.makeChart();
     }
 }
