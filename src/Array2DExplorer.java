933338bbfa5d28fee8fecef920c26d20806d7e5e
BradChadford1 <54950991+BradChadford1@users.noreply.github.com>
2020-02-24 08:51:38 -0800

    fixedd the short circuit error in minEven

--- src/Array2DExplorer.java
+++ src/Array2DExplorer.java
@@ -53,7 +53,7 @@ public class Array2DExplorer {
         int minEven = matrix[0][0];
         for (int r = 0; r < matrix.length; r++) {
             for (int c = 0; c < matrix[0].length; c++) {
-                if ((matrix[r][c] <= minEven) && (matrix[r][c] % 2 == 0))
+                if ((minEven == 1) || ((matrix[r][c] <= minEven) && (matrix[r][c] % 2 == 0)))
                 minEven = matrix[r][c];
             }
         }
@@ -74,19 +74,18 @@ public class Array2DExplorer {
     }
 
     public static void main(String[] args) {
-        int[][] nums = {{-4,2,3,4},{-10,69,1000000,88},{-1,11,111,1111}};
+        int[][] nums = {{1,2,3,4},{1,69,1000000,88},{0,11,111,1111}};
 
-        System.out.println("minRow: " + Array2DExplorer.minRow(nums, 2));
+        System.out.println("minRow     -> expecting: 0\n              received:  " + Array2DExplorer.minRow(nums, 2));
 
-        System.out.println("colsMax: " + Arrays.toString(Array2DExplorer.colsMax(nums)));
+        System.out.println("colsMax    -> expecting: [1, 69, 1000000, 1111]\n              received:  " + Arrays.toString(Array2DExplorer.colsMax(nums)));
 
-        System.out.println("allRowSums: " + Arrays.toString(Array2DExplorer.allRowSums(nums)));
+        System.out.println("allRowSums -> expecting: [10, 1000158, 1233]\n              received:  " + Arrays.toString(Array2DExplorer.allRowSums(nums)));
 
-        System.out.println("colAves: " + Arrays.toString(Array2DExplorer.colAves(nums)));
+        System.out.println("colAves    -> expecting: [0.6666666666666666, 27.333333333333332, 333371.3333333333, 401.0]\n              received:  " + Arrays.toString(Array2DExplorer.colAves(nums)));
 
-        System.out.println("smallEven: " + Array2DExplorer.smallEven(nums));
+        System.out.println("smallEven  -> expecting: 0\n              received:  " + Array2DExplorer.smallEven(nums));
 
-        System.out.println("biggestRow: " + Array2DExplorer.biggestRow(nums));
-
+        System.out.println("biggestRow -> expecting: 1\n              received:  " + Array2DExplorer.biggestRow(nums));
     }
 }
