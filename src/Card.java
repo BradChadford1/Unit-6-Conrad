86a8a3e4a7a8bb700c8aec3a1e7f25f5e51470a9
BradChadford1 <54950991+BradChadford1@users.noreply.github.com>
2020-02-28 09:28:50 -0800

    starting war

---
+++ src/Card.java
@@ -0,0 +1,41 @@
+public class Card implements Comparable{
+    public static final String[] RANK = {"A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2"};
+    public static final String[] SUIT = {"♤", "♡", "♢", "♧"};
+
+    private String rank;
+    private String suit;
+
+    public Card(String rank, String suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    public String toString(){
+        return  this.rank + this.suit;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        Card otherCard = (Card) o;
+        return this.getRankValue() - otherCard.getRankValue();
+    }
+
+
+    public int getRankValue() {
+        for ( int i = 0; i< RANK.length; i++) {
+            if(this.rank.equals(RANK[i]))
+                return 14 - i;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Card c1 = new Card(RANK[0], SUIT[0]);
+        System.out.println(c1);
+        Card c2= new Card(RANK[RANK.length -1], SUIT[SUIT.length-1]);
+        System.out.println(c2);
+        System.out.println(c1.getRankValue());
+        System.out.println(c2.getRankValue());
+        System.out.println(c1.compareTo(c2));
+    }
+}
